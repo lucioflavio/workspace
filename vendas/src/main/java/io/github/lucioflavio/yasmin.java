13aae6cb7fff2b818a2da48e14d8910bccee203f
lucioflavio <lucio.silveira@sysmap.com.br>
2024-05-22 16:54:36 -0300

    commit inicial

---
+++ vendas/src/main/java/io/github/lucioflavio/yasmin.java
@@ -0,0 +1,7 @@
+package io.github.lucioflavio;
+
+public class yasmin {
+    public static void main (String args []){
+        System.out.println("eu sou a sobrinha preferida do tio Lucio");
+    }
+}
